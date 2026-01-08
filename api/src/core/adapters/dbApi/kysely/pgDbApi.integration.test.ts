92210e991415367497d286976a5dfb7e6ccbbc16
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2026-01-08 16:28:40 +0100

    test: fix live data

--- api/src/core/adapters/dbApi/kysely/pgDbApi.integration.test.ts
+++ api/src/core/adapters/dbApi/kysely/pgDbApi.integration.test.ts
@@ -34,7 +34,7 @@ const softwareFormData: SoftwareFormData = {
     softwareDescription: "Super software",
     softwareKeywords: ["bob", "l'éponge"],
     softwareLicense: "MIT",
-    softwareLogoUrl: "https://soft-logo-url.com/logo.png",
+    softwareLogoUrl: "https://external-software-logo-url.com/logo.png",
     softwareName: "",
     softwareType: {
         type: "desktop/mobile",
