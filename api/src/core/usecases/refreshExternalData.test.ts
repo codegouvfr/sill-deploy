17463d6999d78f69a35bd8c129b626d7c762a8aa
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2026-01-15 15:44:07 +0100

    fix: fix tests

--- api/src/core/usecases/refreshExternalData.test.ts
+++ api/src/core/usecases/refreshExternalData.test.ts
@@ -413,7 +413,7 @@ describe("fetches software extra data (from different providers)", () => {
                     license: "Apache License v2.0",
                     logoUrl:
                         "//upload.wikimedia.org/wikipedia/commons/thumb/1/10/Apache_HTTP_server_logo_%282019-present%29.svg/250px-Apache_HTTP_server_logo_%282019-present%29.svg.png",
-                    sourceUrl: "https://svn.apache.org/viewvc/httpd/httpd/",
+                    sourceUrl: "https://github.com/apache/httpd",
                     websiteUrl: "https://httpd.apache.org/",
                     referencePublications: undefined,
                     identifiers: [
